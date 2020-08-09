6be7f35578b62c772cc05475d4a69e1a16f0ee5e
whocares0 <adebayoadewole858@gmail.com>
2020-08-10 00:54:49 +0100

    Updates

---
+++ precache-manifest.fb8fca4d858df20ca8ea7beac02b5f44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d812b7ad23dcb7150c5fd2aa98d54d7",
+    "url": "/frcRoad/index.html"
+  },
+  {
+    "revision": "90069fcecdb91d4940a6",
+    "url": "/frcRoad/static/css/2.803429d5.chunk.css"
+  },
+  {
+    "revision": "0d0e3f65bcae9f88d7b9",
+    "url": "/frcRoad/static/css/main.4120bf42.chunk.css"
+  },
+  {
+    "revision": "90069fcecdb91d4940a6",
+    "url": "/frcRoad/static/js/2.f5418556.chunk.js"
+  },
+  {
+    "revision": "e0546182649f50a2eb923c2a881ed896",
+    "url": "/frcRoad/static/js/2.f5418556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0e3f65bcae9f88d7b9",
+    "url": "/frcRoad/static/js/main.6700fdf6.chunk.js"
+  },
+  {
+    "revision": "8af840d9b87d4f4babb3",
+    "url": "/frcRoad/static/js/runtime-main.0f7d01f1.js"
+  },
+  {
+    "revision": "68dbd93a0cec35df54b6bdf012c0d622",
+    "url": "/frcRoad/static/media/bus.68dbd93a.jpg"
+  }
+]);
